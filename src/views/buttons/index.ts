f5066647388ac320c05831dd2348525bd33111b8
Shane Gatz <shane.gatz@sezzle.com>
2020-12-31 11:02:58 -0600

    update index

---
+++ src/views/buttons/index.ts
@@ -0,0 +1,8 @@
+import ButtonDropdowns from './button-dropdowns/ButtonDropdowns';
+import ButtonGroups from './button-groups/ButtonGroups';
+import Buttons from './buttons/Buttons';
+import BrandButtons from './brand-buttons/BrandButtons';
+
+export {
+  ButtonDropdowns, ButtonGroups, Buttons, BrandButtons
+}
